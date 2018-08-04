25b43bc780291eadcaf64e87032cf055e07e8602
terorie <me@terorie.dev>
2018-08-04 15:52:40 +0200

    Work on JSON video parsing

--- apijson/parsevideo.go
+++ apijson/parsevideo.go
@@ -6,7 +6,13 @@ import (
 	"errors"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
+	"regexp"
 )
+
+var matchThumbUrl = regexp.MustCompile("^.+/hqdefault.jpg")
 
 var missingData = errors.New("missing data")
 var unexpectedType = errors.New("unexpected type")
@@ -26,14 +32,46 @@ func ParseVideo(v *data.Video, res *http.Response) error {
 	rootArray := root.GetArray()
 	if rootArray == nil { return unexpectedType }
 
-	var videoDetails *fastjson.Value
+	// Get interesting objects
+	var pageResponse *fastjson.Value
+	var playerResponse *fastjson.Value
+	var playerArgs *fastjson.Value
 	for _, sub := range rootArray {
-		videoDetails = sub.Get("page", "playerResponse", "videoDetails")
-		if videoDetails != nil { break }
+		if playerResponse == nil {
+			playerResponse = sub.Get("playerResponse")
+			pageResponse = sub.Get("response")
+		}
+		if playerArgs == nil {
+			playerArgs = sub.Get("player", "args")
+		}
+	}
+	if playerResponse == nil { return errors.New("no video details") }
+	if playerArgs == nil { return errors.New("no player args") }
+
+	// Playable in embed?
+	playableInEmbedValue := playerResponse.Get("playabilityStatus", "playableInEmbed")
+	if playableInEmbedValue.Exists() {
+		playableInEmbed, _ := playableInEmbedValue.Bool()
+		v.NoEmbed = !playableInEmbed
 	}
 
+	if err := parseVideoDetails(v, playerResponse.Get("videoDetails"));
+		err != nil { return err }
+
+	watchNextContents := pageResponse.GetArray(
+		"contents", "twoColumnWatchNextResults", "results", "results", "contents")
+	if err := parseVideoInfo(v, watchNextContents);
+		err != nil { return err }
+
+	// Get URL
+	v.URL = string(playerArgs.GetStringBytes("loaderUrl"))
+
+	return nil
+}
+
+func parseVideoDetails(v *data.Video, videoDetails *fastjson.Value) error {
+	// Get tags
 	keywords := videoDetails.GetArray("keywords")
-	if keywords == nil { return missingData }
 	for _, keywordValue := range keywords {
 		keywordBytes, _ := keywordValue.StringBytes()
 		if keywordBytes == nil { continue }
@@ -42,8 +80,59 @@ func ParseVideo(v *data.Video, res *http.Response) error {
 		v.Tags = append(v.Tags, keyword)
 	}
 
-	titleBytes := videoDetails.GetStringBytes("title")
-	if titleBytes != nil { v.Title = string(titleBytes) }
+	// Get title
+	v.Title = string(videoDetails.GetStringBytes("title"))
+	// Get description
+	v.Description = string(videoDetails.GetStringBytes("shortDescription"))
+	// Get channel ID
+	v.UploaderID = string(videoDetails.GetStringBytes("channelId"))
+	if v.UploaderID != "" {
+		v.UploaderURL = "https://www.youtube.com/channel/" + v.UploaderID
+	}
+	// Get author
+	v.Uploader = string(videoDetails.GetStringBytes("author"))
+	// Ratings allowed?
+	v.NoRatings = !videoDetails.GetBool("allowRatings")
+	// Get view count
+	viewCountStr := string(videoDetails.GetStringBytes("viewCount"))
+	viewCount64, err := strconv.ParseUint(viewCountStr, 10, 64)
+	if err == nil { v.Views = viewCount64 }
+	// Get duration
+	lengthStr := string(videoDetails.GetStringBytes("lengthSeconds"))
+	length64, err := strconv.ParseUint(lengthStr, 10, 64)
+	if err != nil { v.Duration = length64 }
+
+	// Get thumbnail URL
+	thumbUrl := string(videoDetails.GetStringBytes("thumbnail", "thumbnails", "0", "url"))
+	if thumbUrl != "" {
+		indices := matchThumbUrl.FindStringIndex(thumbUrl)
+		if len(indices) == 2 {
+			v.Thumbnail = thumbUrl[indices[0]:indices[1]]
+		}
+	}
 
 	return nil
 }
+
+func parseVideoInfo(v *data.Video, videoInfo []*fastjson.Value) error {
+	var primary *fastjson.Value
+	var secondary *fastjson.Value
+
+	for _, obj := range videoInfo {
+		if primary == nil {
+			primary = obj.Get("videoPrimaryInfoRenderer")
+		}
+		if secondary == nil {
+			secondary = obj.Get("videoSecondaryInfoRenderer")
+		}
+	}
+
+	// Get upload date
+	dateText := string(secondary.GetStringBytes("dateText", "simpleText"))
+	dateText = strings.TrimPrefix(dateText, "Published on ")
+	date, err := time.Parse("Jan _2, 2006", dateText)
+	if err == nil { v.UploadDate = data.SimpleTime(date) }
+
+	return nil
+}
+
