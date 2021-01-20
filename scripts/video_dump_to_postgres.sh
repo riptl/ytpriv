ce6a6cb66d98a221a5f00555ea206b67c7617308
terorie <me@terorie.dev>
2021-01-20 21:20:41 +0100

    remove scripts

--- scripts/video_dump_to_postgres.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-jq -r '"INSERT INTO videos (id, uploader_id, duration, genre) VALUES ($$\(.id)$$, $$\(.uploader_id)$$, \(.duration), $$\(.genre)$$) ON CONFLICT DO NOTHING;"' $@
--- scripts/video_dump_to_tsv.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-jq -r '"\(.id)\t\(.uploader_id)\t\(.duration)\t\(.genre)"' $@
